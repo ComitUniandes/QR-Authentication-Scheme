ccfbeb6e68c1c0bb8a4145cd9b8cc34d3e7dec3f
Andrea Paredes <aj.paredes10@uniandes.edu.co>
2019-12-07 16:44:08 -0500

    initial commit https

--- Server/src/main/java/com/ajparedes/SecurityConfig.java
+++ Server/src/main/java/com/ajparedes/SecurityConfig.java
@@ -1,15 +1,60 @@
 package com.ajparedes;
 
 import java.security.SecureRandom;
+import java.util.Arrays;
 import java.util.Base64;
 import java.util.Base64.Encoder;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Value(value = "${auth0.apiAudience}")
+    private String apiAudience;
+    @Value(value = "${auth0.issuer}")
+    private String issuer;
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("https://192.168.0.4:8080"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedHeader("Authorization");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        JwtWebSecurityConfigurer
+                .forRS256(apiAudience, issuer)
+                .configure(http)
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/users").authenticated()
+                .antMatchers(HttpMethod.POST, "/users/login").permitAll()
+                .antMatchers(HttpMethod.GET, "/devices/list").authenticated()
+                .antMatchers(HttpMethod.POST, "/devices/unlink").authenticated()
+                .antMatchers(HttpMethod.POST, "/tokens/generate").authenticated()
+                .antMatchers(HttpMethod.POST, "/tokens/validate").permitAll();
+        
+    }
+	
 	@Bean 
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
